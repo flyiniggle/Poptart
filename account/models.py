24a027b9ba7895bddcc608a9fe0bb18ad80a4b6f
Dan Thompson <dthompson@vestmark.com>
2015-09-04 14:27:44 -0400

    add __str__ and __unicode__ to models

--- account/models.py
+++ account/models.py
@@ -5,8 +5,20 @@ class Account(models.Model):
     name = models.CharField(unique=True, max_length=100)
     holdings = models.ForeignKey('Holding')
 
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
 
 class Holding(models.Model):
     security = models.ForeignKey('securitymanager.Security')
     quantity = models.FloatField()
     expected_quantity = models.FloatField()
+
+    def __str__(self):
+        return str("%s, %s" % (self.security, self.quantity))
+
+    def __unicode__(self):
+        return unicode("%s, %s" % (self.security, self.quantity))
--- securitymanager/models.py
+++ securitymanager/models.py
@@ -15,8 +15,20 @@ class Segment(models.Model):
     segment = models.CharField(max_length=1, choices=SEGMENT_CHOICES)
     name = models.CharField(unique=True, max_length=100)
 
+    def __str__(self):
+        return str(self.name)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
 
 class Security(models.Model):
     ticker = models.CharField(unique=True, max_length=2)
     CUSIP = models.CharField(unique=True, max_length=9)
     segment = models.ForeignKey('Segment')
+
+    def __str__(self):
+        return str(self.ticker)
+
+    def __unicode__(self):
+        return unicode(self.ticker)
