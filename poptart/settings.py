41d78a8e2674cd8facc263f261dd21b03cec8224
Dan Thompson <dthompson@vestmark.com>
2015-09-04 10:08:11 -0400

    db setup and first migration

--- poptart/settings.py
+++ poptart/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'poptart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'poptarts',
+        'USER': 'root'
     }
 }
 
