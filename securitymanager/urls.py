2bff5cd3c948e34aa394cd2f7baae443e52ad314
Dan Thompson <dthompson@vestmark.com>
2015-09-22 15:59:10 -0400

    Basic securities API

--- securitymanager/urls.py
+++ securitymanager/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.securities, name="securities"),
+    url(r'^(?P<ticker>[a-zA-Z]+)/$', views.security, name="security")
 ]
--- securitymanager/views.py
+++ securitymanager/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from poptart.lib.serializers import JSONSerializer
+from securitymanager.models import Security, AssetClass
 
-def index(request):
-    return HttpResponse("This is some text")
+
+def securities(request):
+    return HttpResponse(JSONSerializer().serialize(Security.objects.all()))
+
+
+def security(request, ticker):
+    return HttpResponse(JSONSerializer().serialize(Security.objects.filter(ticker=ticker)))
