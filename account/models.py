9855a49500f71b7646575d3af212149870840361
Dan Thompson <dthompson@vestmark.com>
2015-09-22 09:19:17 -0400

    Add account description

--- account/models.py
+++ account/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Account(models.Model):
     name = models.CharField(unique=True, max_length=100)
+    description = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return str(self.name)
