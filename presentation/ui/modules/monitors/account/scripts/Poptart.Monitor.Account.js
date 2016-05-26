7ab54c0ebf9b89433864bc7bd5c1c7b17eedc94d
Dan Thompson <dthompson@vestmark.com>
2016-05-26 11:54:37 -0400

    deep-six infragistics (again) and add a basic paramquery table

--- presentation/ui/modules/monitors/account/scripts/Poptart.Monitor.Account.js
+++ presentation/ui/modules/monitors/account/scripts/Poptart.Monitor.Account.js
@@ -13,57 +13,33 @@ Poptart.Monitor.Account = function(){
 			source: Poptart.Monitor.Account.accountList
 		});
 
-		testTable.igGrid({
-			dataSource: Poptart.Monitor.Account.treegridData,
-			primaryKey: "pk",
+		testTable.pqGrid({
+			dataModel: {
+				data: Poptart.Monitor.Account.treegridData,
+				type: "local",
+				sorting: "local"
+			},
 			width: "100%",
-			defaultColumnWidth: "150px",
-			columns: [
-				{headerText: "#", key: "pk", dataType: "number", width: "40px"},
-				{headerText: "Name", key: "name", dataType: "string", width: "175px"},
-				{headerText: "Description", key: "description", dataType: "string", width: "150px"},
-				{headerText: "Cash", key: "total_cash", dataType: "number", width: "100px"},
-				{headerText: "Value", key: "total_value", dataType: "number", width: "100px"},
-				{headerText: "Expected Cash", key: "expected_cash", dataType: "number", width: "150px"},
-				{headerText: "Expected Value", key: "total_expected_value", dataType: "number", width: "150px"},
-				{headerText: "Cash Drift", key: "cash_drift", dataType: "number", width: "100px"},
-				{headerText: "Holdings Drift", key: "holdings_drift", dataType: "number", width: "100px"},
-				{headerText: "Total Drift", key: "total_drift", dataType: "number", width: "100px"},
-				{headerText: "Max Cash Drift", key: "max_cash_drift", dataType: "number", width: "150px"},
-				{headerText: "Max Position Drift", key: "max_pos_drift", dataType: "number", width: "150px"},
-				{headerText: "Max Total Drift", key: "max_total_drift", dataType: "number", width: "150px"},
-				{headerText: "Client", key: "client_1_id", dataType: "string"},
-				{headerText: "Manager", key: "manager", dataType: "string"},
-				{headerText: "Start Date", key: "inception_date", dataType: "date"},
-				{headerText: "Solution", key: "solution_name", dataType: "string"},
-				{headerText: "Last Updated", key: "last_update", dataType: "date"}
-			],
-			features:[
-				{
-					name: "Paging"
-				},
-				{
-					name: "GroupBy",
-					groupByDialogContainment: "window",
-					columnSettings:[
-						{
-							columnKey: "total_cash",
-							summaries: [
-								{
-									summaryFunction:"custom",
-									text: "Total cash:",
-									customSummary: function(data){
-										var cash = 0;
-										$.map(data, function(d){
-											cash += parseFloat(d);
-										});
-										return cash;
-									}
-								}
-							]
-						}
-					]
-				}
+			flexHeight: true,
+			colModel: [
+				{title: "#", dataIndx: "pk", dataType: "integer", width: "40px"},
+				{title: "Name", dataIndx: "name", dataType: "string", width: "175px"},
+				{title: "Description", dataIndx: "description", dataType: "string", width: "150px"},
+				{title: "Cash", dataIndx: "total_cash", dataType: "float", width: "100px"},
+				{title: "Value", dataIndx: "total_value", dataType: "float", width: "100px"},
+				{title: "Expected Cash", dataIndx: "expected_cash", dataType: "float", width: "150px"},
+				{title: "Expected Value", dataIndx: "total_expected_value", dataType: "float", width: "150px"},
+				{title: "Cash Drift", dataIndx: "cash_drift", dataType: "float", width: "100px"},
+				{title: "Holdings Drift", dataIndx: "holdings_drift", dataType: "float", width: "100px"},
+				{title: "Total Drift", dataIndx: "total_drift", dataType: "float", width: "100px"},
+				{title: "Max Cash Drift", dataIndx: "max_cash_drift", dataType: "float", width: "150px"},
+				{title: "Max Position Drift", dataIndx: "max_pos_drift", dataType: "float", width: "150px"},
+				{title: "Max Total Drift", dataIndx: "max_total_drift", dataType: "float", width: "150px"},
+				{title: "Client", dataIndx: "client_1_id", dataType: "string"},
+				{title: "Manager", dataIndx: "manager", dataType: "string"},
+				{title: "Start Date", dataIndx: "inception_date", dataType: "date"},
+				{title: "Solution", dataIndx: "solution_name", dataType: "string"},
+				{title: "Last Updated", dataIndx: "last_update", dataType: "date"}
 			]
 		});
 
