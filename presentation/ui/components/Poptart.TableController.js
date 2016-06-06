e0de6419ff737bafaffcf48665bd6ec9cb16f28a
Dan Thompson <dthompson@vestmark.com>
2016-06-06 13:38:56 -0400

    not the worst possible implementation of controlling the table page settings programmatically. certainly an effective POC.

--- presentation/ui/components/Poptart.TableController.js
+++ presentation/ui/components/Poptart.TableController.js
@@ -6,17 +6,18 @@ Poptart.TableController = function(table){
 		var self = this;
 
 		// Static
-		self.currentPageIndex = ko.observable(0);
-		self.pageSizeOptions = ko.observableArray(["10", "25", "50", "100", "500", "1000"]);
-		self.pageSize = ko.observable("10");
+		self.currentPageIndex = ko.observable(table.pqGrid("option", "pageModel.curPage"));
+		self.pageSizeOptions = ko.observableArray(table.pqGrid("option", "pageModel.rPPOptions"));
+		self.pageSize = ko.observable(table.pqGrid("option", "pageModel.rPP"));
 
 		// Computed
 		self.currentPage = ko.computed({
 			read: function() {
-				return self.currentPageIndex() + 1;
+				return parseInt(table.pqGrid("option", "pageModel.curPage"));
 			},
 			write: function(value) {
-					self.currentPageIndex((value - 1));
+					self.currentPageIndex((value));
+					table.pqGrid("refreshDataAndView");
 				}
 			}
 		);
@@ -30,11 +31,12 @@ Poptart.TableController = function(table){
 	ko.applyBindings(model, document.getElementById("tableControlsHeader"));
 
 	model.pageSize.subscribe(function(value) {
-		table.igGridPaging("pageSize", value);
+		table.pqGrid("option", "pageModel.rPP", parseInt(value));
+		table.pqGrid("refreshDataAndView");
 	});
 
 	model.currentPageIndex.subscribe(function(value) {
-		table.igGridPaging("pageIndex", value);
+		table.pqGrid("goToPage", {page: value});
 	});
 
 };
