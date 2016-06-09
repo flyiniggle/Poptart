a0ddd9f9fea154736b05615add2f52eae71513f0
Dan Thompson <dthompson@vestmark.com>
2016-06-09 16:58:37 -0400

    table POC for Kendo

--- presentation/support/Kendo/DataAdapter.js
+++ presentation/support/Kendo/DataAdapter.js
@@ -3,18 +3,16 @@ module.exports = function() {
 	var dataAdapter = {};
 
 	dataAdapter.translateServerResponse = function(params, data) {
-
 		return {
-			currentPage: params.pq_curpage,
-			totalRecords: data.total_accounts,
+			total: data.total_accounts,
 			data: data.accounts_data
 		}
 	};
 
 	dataAdapter.translateServerRequest = function(params) {
 		return {
-			current_page: params.pq_curpage,
-			page_size: params.pq_rpp
+			current_page: params.page,
+			page_size: params.pageSize
 		}
 	};
 
