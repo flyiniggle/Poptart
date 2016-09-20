a90651df61e592ee4d6831aa92865c4e0ac094d6
Dan Thompson <dthompson@vestmark.com>
2016-09-20 07:51:50 -0400

    slightly improved API test

--- account/tests/integrationtests.py
+++ account/tests/integrationtests.py
@@ -13,5 +13,7 @@ class AccountMonitorTest(TestCase):
             content = simplejson.loads(response.content)
             self.assertTrue("total_accounts" in content, "No 'total_accounts' field was found.")
             self.assertTrue("accounts_data" in content, "No 'accounts_data' field was found.")
+            self.assertIsInstance(content.get("total_accounts"), int, "The 'total_accounts' field was not an int.")
+            self.assertIsInstance(content.get("accounts_data"), list, "The 'accounts_data' field was not a list.")
         except simplejson.JSONDecodeError as e:
             self.fail("Response was not properly formatted JSON: %s. Exception: %s" % (response.content, e.message))
