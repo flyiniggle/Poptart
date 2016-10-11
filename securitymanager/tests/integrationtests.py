af6274caee0db34619591263affaf5b0e53d2e94
Dan Thompson <dthompson@vestmark.com>
2016-10-11 12:04:09 -0400

    test for getting a single security

--- securitymanager/tests/integrationtests.py
+++ securitymanager/tests/integrationtests.py
@@ -34,3 +34,22 @@ class SecurityManagerTest(TestCase):
 
         self.assertEqual(response.status_code, "200 OK", "Response status was not OK: {0}.".format(response.status_code))
         self.assertEqual(content.get("total_count"), 213, "Summary's total count returned {0} instead of 213.".format(content.get("total_count")))
+
+    def test_get_security(self):
+        c = Client()
+        response = c.get('/securities/F/')
+        expected_data = {'CUSIP': '345370860', 'description': 'Ford Motor', 'ticker': 'F', 'last_price': '16.00', 'segment': 13}
+
+        try:
+            content = simplejson.loads(response.content)
+        except simplejson.JSONDecodeError as e:
+            self.fail("Response was not properly formatted JSON: %s. Exception: %s." % (response.content, e.message))
+
+        self.assertEqual(response.status_code, "200 OK", "Response status was not OK: {0}.".format(response.status_code))
+        self.assertEqual(len(content), 1, "Get security did not return only one result: %s" % response.content)
+        sec = content[0]
+        self.assertEqual(sec.get("CUSIP"), expected_data.get("CUSIP"), "CUSIPS did not match. Expected %s and got %s." % (expected_data.get("CUSIP"), sec.get("CUSIP")))
+        self.assertEqual(sec.get("description"), expected_data.get("description"), "Descriptions did not match. Expected %s and got %s." % (expected_data.get("description"), sec.get("description")))
+        self.assertEqual(sec.get("ticker"), expected_data.get("ticker"), "Tickers did not match. Expected %s and got %s." % (expected_data.get("ticker"), sec.get("ticker")))
+        self.assertEqual(sec.get("last_price"), expected_data.get("last_price"), "Prices did not match. Expected %s and got %s." % (expected_data.get("last_price"), sec.get("last_price")))
+        self.assertEqual(sec.get("segment"), expected_data.get("segment"), "Segments did not match. Expected %s and got %s." % (expected_data.get("segment"), sec.get("segment")))
