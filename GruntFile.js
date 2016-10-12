c7e2fb9ad231ff0dcf270358d5390977eb34b42c
Dan Thompson <dthompson@vestmark.com>
2016-10-12 12:06:44 -0400

    lint as part of default grunt task

--- GruntFile.js
+++ GruntFile.js
@@ -12,10 +12,19 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		eslint: {
+			options: {
+				configFile: 'presentation/build/eslint.json',
+				ignorePath: 'presentation/build/.eslintignore'
+			},
+			target: ['presentation/**/*.js', '!presentation/ui/scripts/*']
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-shell');
-	grunt.registerTask('default', ['test']);
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.registerTask('default', ['lint', 'test']);
 	grunt.registerTask('test', ['shell:test']);
+	grunt.registerTask('lint', ['eslint']);
 
 };
