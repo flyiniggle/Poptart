2b8280ce22b46bc00e707b13fc2543ef19973976
Dan Thompson <dthompson@vestmark.com>
2016-12-06 13:16:56 -0500

    clean up build failures

--- presentation/test/client/test.js
+++ presentation/test/client/test.js
@@ -1,3 +1,5 @@
+var assert = chai.assert;
+
 describe("Poptart", function() {
 	describe("#basic", function() {
 		it("should return a map", function() {
--- presentation/test/server/test.js
+++ presentation/test/server/test.js
@@ -6,7 +6,7 @@ global.imports = function(name) {
 var path = require('path');
 
 var chai = require('chai');
-var sinon = require('sinon');
+//var sinon = require('sinon');
 var assert = chai.assert;
 
 const igniteDataAdapter = imports('/support/Ignite/DataAdapter.js');
@@ -14,14 +14,14 @@ const igniteDataAdapter = imports('/support/Ignite/DataAdapter.js');
 describe("Ignite DataAdapter", function() {
 	describe("#translateServerRequest", function() {
 		const testParams = {
-				current_page: 0,
-				page_size: 100
-			};
+			current_page: 0,
+			page_size: 100
+		};
 
 		it("should increment the current page parameter by 1", function() {
 			var pageForServer = igniteDataAdapter.translateServerRequest(testParams);
 
-			chai.assert.strictEqual(pageForServer.current_page,
+			assert.strictEqual(pageForServer.current_page,
 									1,
 									"current_page parameter for server was not incremented correctly.");
 		});
@@ -40,41 +40,41 @@ describe("Alert", function() {
 	it("should return a map", function() {
 		var alert = new Alert(testInfo.level, testInfo.summary, testInfo.message);
 
-		chai.assert.isObject(alert, "Alert did not return a map.");
+		assert.isObject(alert, "Alert did not return a map.");
 	});
 
 	it("should have the correct attributes", function() {
 		var alert = new Alert(testInfo.level, testInfo.summary, testInfo.message);
 
-		chai.assert.propertyVal(alert, "level", testInfo.level, "Level did not match expected.");
-		chai.assert.propertyVal(alert, "summary", testInfo.summary, "Summary did not match expected.");
-		chai.assert.propertyVal(alert, "message", testInfo.message, "Message did not match expected.");
+		assert.propertyVal(alert, "level", testInfo.level, "Level did not match expected.");
+		assert.propertyVal(alert, "summary", testInfo.summary, "Summary did not match expected.");
+		assert.propertyVal(alert, "message", testInfo.message, "Message did not match expected.");
 	});
 
 	it("should be info", function() {
 		var alert = new Alert("info");
 
-		chai.assert.propertyVal(alert, "level", "info", "Level did not match expected: info.");
+		assert.propertyVal(alert, "level", "info", "Level did not match expected: info.");
 	});
 
 	it("should be warning", function() {
 		var alert = new Alert("warning");
 
-		chai.assert.propertyVal(alert, "level", "warning", "Level did not match expected: warning.");
+		assert.propertyVal(alert, "level", "warning", "Level did not match expected: warning.");
 	});
 
 	it("should be error", function() {
 		var alert = new Alert("error");
 
-		chai.assert.propertyVal(alert, "level", "error", "Level did not match expected: error.");
+		assert.propertyVal(alert, "level", "error", "Level did not match expected: error.");
 	});
 
 	it("should throw an error if level is not valid", function() {
-		chai.assert.throws(function() {
-								new Alert("green", testInfo.summary, testInfo.message);
-							},
-						   "Invalid level.",
-						   "Alert did not throw an error when constructed with an invalid level");
+		assert.throws(function() {
+			new Alert("green", testInfo.summary, testInfo.message);
+		},
+	   "Invalid level.",
+	   "Alert did not throw an error when constructed with an invalid level");
 	});
 
 	describe("#stringify", function() {
@@ -82,8 +82,8 @@ describe("Alert", function() {
 			var alert = new Alert(testInfo.level, testInfo.summary, testInfo.message),
 				alertString = alert.stringify();
 
-			chai.assert.isString(alertString, "Stringify did not return a string.");
-			chai.assert.doesNotThrow(function() {
+			assert.isString(alertString, "Stringify did not return a string.");
+			assert.doesNotThrow(function() {
 				JSON.parse(alertString);
 			}, Error, "Result of stringify could not be parsed as JSON.");
 		});
