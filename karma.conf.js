8e775f9f99700c66527dee2738dd74f4ebee184e
Dan Thompson <dthompson@vestmark.com>
2017-01-25 16:33:27 -0500

    formatting and better script dependency loading for browser tests

--- karma.conf.js
+++ karma.conf.js
@@ -2,72 +2,60 @@
 // Generated on Mon Nov 28 2016 09:16:47 GMT-0500 (Eastern Standard Time)
 
 module.exports = function(config) {
-  config.set({
+	config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './presentation',
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: './presentation',
 
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['mocha', 'chai', 'sinon', 'jquery-chai'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon', 'jquery-chai'],
+		// list of files / patterns to load in the browser
+		files: [
+			'static/ui/scripts/*.js',
+			'static/ui/poptart.min.js',
+			'static/ui/**/*.min.js',
+			'static/ui/components/**/*.min.js',
+			'static/ui/modules/**/*.min.js',
+			'test/client/*.js'
+		],
 
+		// list of files to exclude
+		exclude: [],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'static/ui/scripts/*.js',
-      'static/ui/*.min.js',
-      'static/ui/components/**/*.min.js',
-      'static/ui/modules/**/*.min.js',
-      'test/client/*.js'
-    ],
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {},
 
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress'],
 
-    // list of files to exclude
-    exclude: [
-    ],
+		// web server port
+		port: 9000,
 
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['Chrome', 'PhantomJS'],
 
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
 
-    // web server port
-    port: 9000,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	});
+};
