178acb27d12d87c3671d5ffad1d3d7bb39d2c00a
Dan Thompson <dthompson@vestmark.com>
2017-02-23 09:50:59 -0500

    securities table now works with ig loader

--- presentation/ui/modules/monitors/security/scripts/Poptart.Monitor.Security.js
+++ presentation/ui/modules/monitors/security/scripts/Poptart.Monitor.Security.js
@@ -2,20 +2,26 @@ Poptart.Monitor.Security = function() {
 	var ReturnObj = {};
 
 	ReturnObj.init = function() {
-		var table = jQuery("#sm_table");
+		var loaderConfig = Object.create(Poptart.Ignite.loaderConfig, {});
 
-		table.igGrid({
-			dataSource: Poptart.Monitor.Security.securities,
-			width: "100%",
-			columns: [
-				{headerText: "#", key: "pk", dataType: "integer", width: "40px"},
-				{headerText: "Ticker", key: "ticker", dataType: "string", width: "150px"},
-				{headerText: "CUSIP", key: "CUSIP", dataType: "string", width: "150px"},
-				{headerText: "Description", key: "description", dataType: "string", width: "200px"},
-				{headerText: "Last Price", key: "last_price", dataType: "float", width: "100px"},
-				{headerText: "Asset Class", key: "segment", dataType: "float", width: "75px"}
-			]
-		});
+		loaderConfig.resources = "igGrid";
+		loaderConfig.ready = function() {
+			var table = jQuery("#sm_table");
+
+			table.igGrid({
+				dataSource: Poptart.Monitor.Security.securities,
+				width: "100%",
+				columns: [
+					{headerText: "#", key: "pk", dataType: "integer", width: "40px"},
+					{headerText: "Security", key: "ticker", dataType: "string", width: "150px"},
+					{headerText: "CUSIP", key: "CUSIP", dataType: "string", width: "150px"},
+					{headerText: "Description", key: "description", dataType: "string", width: "200px"},
+					{headerText: "Last Price", key: "last_price", dataType: "float", width: "100px"},
+					{headerText: "Asset Class", key: "segment", dataType: "float", width: "75px"}
+				]
+			});
+		};
+		jQuery.ig.loader(loaderConfig);
 	};
 
 	return ReturnObj;
