a032df4f4576c108151eec5b5ab2107e7047c2eb
Dan Thompson <dthompson@vestmark.com>
2017-05-22 13:21:51 -0400

    use the new adding functionality on account creation; start making a holdings chart

--- presentation/ui/modules/monitors/account/scripts/Poptart.Monitor.Account.CreateAccount.js
+++ presentation/ui/modules/monitors/account/scripts/Poptart.Monitor.Account.CreateAccount.js
@@ -3,7 +3,6 @@ Poptart.Monitor.Account.CreateAccount = function() {
 
 	//View Models
 	//////////////////
-
 	var AccountCreationViewModel = function() {
 		var self = this;
 
@@ -43,66 +42,8 @@ Poptart.Monitor.Account.CreateAccount = function() {
 		self.cashDrift = ko.computed(function() {
 			return Math.abs(self.expectedCash() - self.startingCash());
 		}, self).extend({CurrencyDisplay: null});
-
-		//Methods
-		self.addHolding = function() {
-			this.holdings.push(new HoldingModel());
-			jQuery("#accountAddHoldings").igGrid("commit");
-		};
 	};
 
-	var HoldingModel = function() {
-		var pk = 0;
-
-		return function() {
-			var self = this;
-
-			self.pk = ko.observable(pk++);
-			self.securityName = ko.observable("");
-			self.securityId = ko.observable(0);
-			self.segment = ko.observable("");
-			self.securityLastPrice = ko.observable(0);
-			self.quantity = ko.observable(0);
-			self.expectedQuantity = ko.observable(0);
-			self.expectedValue = ko.observable(0);
-
-			self.quantityDrift = ko.pureComputed({
-				read: function() {
-					return Math.abs(this.expectedQuantity() - this.quantity());
-				},
-				write: function() {
-					return undefined;
-				},
-				owner: self
-			}, self);
-			self.valueDrift = ko.pureComputed({
-				read: function() {
-					return Math.abs(this.expectedValue() - this.value());
-				},
-				write: function() {
-					return undefined;
-				},
-				owner: self
-			}, self);
-			self.value = ko.pureComputed({
-				read: function() {
-					return this.quantity() * this.securityLastPrice();
-				},
-				write: function() {
-					return undefined;
-				},
-				owner: self
-			});
-
-			//Because Ignite UI made me do it :/
-			self.update = function(security) {
-				self.securityName(security.ticker);
-				self.securityId(security.pk);
-				self.segment(security.segment);
-				self.securityLastPrice(security.last_price);
-			};
-		};
-	}();
 
 	// Ig Component/Knockout Configuration
 	/////////////////////////////////////////
@@ -111,22 +52,153 @@ Poptart.Monitor.Account.CreateAccount = function() {
 		this.autoCommit = true;
 		this.autoGenerateColumns = false;
 		this.columns = [
-			{headerText: 'pk', key: "pk", dataType: "number", hidden: true},
-			{headerText: "Name", key: "securityName", dataType: "string", width: "120px"},
-			{headerText: "Id", key: "securityId", dataType: "number", width: "*"},
-			{headerText: "Segment", key: "segment", dataType: "string", width: "*"},
-			{headerText: "Quantity", key: "quantity", dataType: "number", width: "*"},
-			{headerText: "Value", key: "value", dataType: "number", width: "*"},
-			{headerText: "Expected Quantity", key: "expectedQuantity", dataType: "number", width: "*"},
-			{headerText: "Expected Value", key: "expectedValue", dataType: "number", width: "*"},
-			{headerText: "Quantity Drift", key: "quantityDrift", dataType: "number", width: "*"},
-			{headerText: "Value Drift", key: "valueDrift", dataType: "number", width: "*"},
-			{headerText: "Price", key: "securityLastPrice", dataType: "number", width: "*"}
+			{
+				headerText: "Name",
+				key: "security",
+				dataType: "object",
+				width: "150px",
+				mapper: function(row) {
+					var securityObject = row.security || {};
+
+					return securityObject.ticker || "";
+				}
+			},
+			{
+				headerText: "CUSIP",
+				key: "CUSIP",
+				dataType: "string",
+				width: "80px"
+			},
+			{
+				headerText: "Description",
+				key: "description",
+				dataType: "number",
+				width: "200px"
+			},
+			{headerText: "Quantity", key: "quantity", dataType: "number", width: "80px"},
+			{
+				headerText: "Value",
+				key: "value",
+				dataType: "number",
+				unbound: true,
+				width: "80px",
+				formula: function(row) {
+					return (row.securityLastPrice || 0) * row.quantity;
+				}
+			},
+			{headerText: "Expected Quantity", key: "expectedQuantity", dataType: "number", width: "80px"},
+			{headerText: "Expected Value", key: "expectedValue", dataType: "number", width: "100px"},
+			{
+				headerText: "Quantity Drift",
+				key: "quantityDrift",
+				dataType: "number",
+				width: "100px",
+				unbound: true,
+				formula: function(row) {
+					return Math.abs(row.quantity - row.expectedQuantity);
+				}
+			},
+			{
+				headerText: "Value Drift",
+				key: "valueDrift",
+				dataType: "number",
+				width: "100px",
+				unbound: true,
+				formula: function(row) {
+					return Math.abs(((row.securityLastPrice || 0) * row.quantity) - row.expectedValue);
+				}
+			},
+			{headerText: "Segment", key: "segment", dataType: "string", width: "100px"},
+			{headerText: "Price", key: "securityLastPrice", dataType: "number", width: "100px"},
+			{headerText: 'pk', key: "pk", dataType: "number", hidden: true}
 		];
 		this.features = [
 			{
+				name: "Adding",
+				newRowFormatter: function(row) {
+					return {
+						security: row.security,
+						CUSIP: row.security.CUSIP,
+						description: row.security.description,
+						quantity: row.quantity,
+						expectedQuantity: row.expectedQuantity,
+						expectedValue: row.expectedValue,
+						segment: row.security.segment,
+						securityLastPrice: row.security.last_price,
+						pk: row.security.pk
+					};
+				},
+				columnSettings: [
+					{
+						columnKey: "security",
+						readOnly: false,
+						default: null,
+						required: true,
+						editorType: "combo",
+						editorOptions: setComboEditorOptions.call({}),
+						mapper: function(security) {
+							return security ? security.ticker : "";
+						}
+					},
+					{
+						columnKey: "CUSIP",
+						readOnly: true,
+						formula: function(row) {
+							var securityObject = row.security || {};
+
+							return securityObject.CUSIP || "";
+						}
+					},
+					{
+						columnKey: "description",
+						formula: function(row) {
+							var securityObject = row.security || {};
+
+							return securityObject.description || "";
+						}
+					},
+					{
+						columnKey: "value",
+						readOnly: true,
+						formula: function(row) {
+							var securityObject = row.security || {};
+
+							return (securityObject.last_price || 0) * row.quantity;
+						}
+					},
+					{columnKey: "quantityDrift", readOnly: true},
+					{
+						columnKey: "valueDrift",
+						readOnly: true,
+						formula: function(row) {
+							var securityObject = row.security || {};
+
+							return Math.abs(((securityObject.last_price || 0) * row.quantity) - row.expectedValue);
+						}
+					},
+					{
+						columnKey: "securityLastPrice",
+						readOnly: true,
+						formula: function(row) {
+							var securityObject = row.security || {};
+
+							return securityObject.last_price || "";
+						}
+					},
+					{
+						columnKey: "segment",
+						readOnly: true,
+						formula: function(row) {
+							var securityObject = row.security || {};
+
+							return securityObject.segment || "";
+						}
+					}
+				]
+			},
+			{
 				name: "Updating",
-				editMode: "row",
+				editMode: "cell",
 				enableAddRow: false,
 				enableDeleteRow: true,
 				autoCommit: false,
@@ -140,55 +212,18 @@ Poptart.Monitor.Account.CreateAccount = function() {
 					}
 				},
 				columnSettings: [
-					{
-						columnKey: "pk",
-						readOnly: true
-					},
-					{
-						columnKey: "securityId",
-						readOnly: true
-					},
-					{
-						columnKey: "securityName",
-						readOnly: false,
-						editorType: "combo",
-						editorOptions: setComboEditorOptions.call({})
-					},
-					{
-						columnKey: "segment",
-						readOnly: true
-					},
-					{
-						columnKey: "quantityDrift",
-						readOnly: true
-					},
-					{
-						columnKey: "value",
-						readOnly: true
-					},
-					{
-						columnKey: "valueDrift",
-						readOnly: true
-					},
-					{
-						columnKey: "securityLastPrice",
-						readOnly: true
-					},
-					{
-						columnKey: "quantity",
-						editorType: "numeric",
-						readOnly: false
-					},
-					{
-						columnKey: "expectedQuantity",
-						editorType: "numeric",
-						readOnly: false
-					},
-					{
-						columnKey: "expectedValue",
-						editorType: "currency",
-						readOnly: false
-					}
+					{columnKey: "security", readOnly: true},
+					{columnKey: "CUSIP", readOnly: true},
+					{columnKey: "description", readOnly: true},
+					{columnKey: "quantity", editorType: "numeric", readOnly: false},
+					{columnKey: "value", readOnly: true},
+					{columnKey: "quantityDrift", readOnly: true},
+					{columnKey: "valueDrift", readOnly: true},
+					{columnKey: "securityLastPrice", readOnly: true},
+					{columnKey: "expectedQuantity", editorType: "numeric", readOnly: false},
+					{columnKey: "expectedValue", editorType: "currency", readOnly: false},
+					{columnKey: "segment", readOnly: true},
+					{columnKey: "pk", readOnly: true}
 				]
 			}
 		];
@@ -219,7 +254,9 @@ Poptart.Monitor.Account.CreateAccount = function() {
 		this.textKey = "ticker";
 		this.valueKey = "pk";
 		this.autoComplete = true;
-		this.mode = 'dropdown';
+		this.autoSelectFirstMatch = false;
+		this.selectItemBySpaceKey = true;
+		this.mode = 'editable';
 		this.dataSource = new jQuery.ig.DataSource({
 			dataSource: "/monitors/account/securities",
 			type: "remoteUrl",
@@ -229,7 +266,8 @@ Poptart.Monitor.Account.CreateAccount = function() {
 					{name: "ticker", type: "string"},
 					{name: "CUSIP", type: "number"},
 					{name: "segment", type: "string"},
-					{name: "last_price", type: "number"}
+					{name: "last_price", type: "number"},
+					{name: "description", type: "string"}
 				]
 			})
 		});
@@ -241,24 +279,31 @@ Poptart.Monitor.Account.CreateAccount = function() {
 	//////////////////
 
 	ReturnObj.init = function() {
+		var loaderConfig = Object.create(Poptart.Ignite.loaderConfig, {});
 
-		viewModel = new AccountCreationViewModel();
+		loaderConfig.resources = "igCombo,igEditors,igGrid.Updating.Adding,extensions/infragistics.datasource.knockoutjs.js,extensions/infragistics.ui.grid.knockout-extensions.js,extensions/infragistics.ui.combo.knockout-extensions.js,extensions/infragistics.ui.editors.knockout-extensions.js";
+		loaderConfig.ready = function() {
+			viewModel = new AccountCreationViewModel();
 
-		//append ig component options
-		viewModel.holdingsGridOptions = setGridOptions.call({
-			primaryKey: "pk",
-			dataSource: viewModel.holdings
-		});
-		viewModel.expectedCashEditorOptions = setCurrencyEditorOptions.call({value: viewModel.expectedCash});
-		viewModel.startingCashEditorOptions = setCurrencyEditorOptions.call({value: viewModel.startingCash});
-		viewModel.maxCashDriftEditorOptions = setCurrencyEditorOptions.call({value: viewModel.maxCashDrift});
-		viewModel.maxPositionDriftEditorOptions = setCurrencyEditorOptions.call({value: viewModel.maxPositionDrift});
-		viewModel.maxTotalDriftEditorOptions = setCurrencyEditorOptions.call({value: viewModel.maxTotalDrift});
-		viewModel.maxCashDriftPercentEditorOptions = setPercentEditorOptions.call({value: viewModel.maxCashDriftPercent});
+			//append ig component options
+			viewModel.expectedCashEditorOptions = setCurrencyEditorOptions.call({value: viewModel.expectedCash});
+			viewModel.startingCashEditorOptions = setCurrencyEditorOptions.call({value: viewModel.startingCash});
+			viewModel.maxCashDriftEditorOptions = setCurrencyEditorOptions.call({value: viewModel.maxCashDrift});
+			viewModel.maxPositionDriftEditorOptions = setCurrencyEditorOptions.call({value: viewModel.maxPositionDrift});
+			viewModel.maxTotalDriftEditorOptions = setCurrencyEditorOptions.call({value: viewModel.maxTotalDrift});
+			viewModel.maxCashDriftPercentEditorOptions = setPercentEditorOptions.call({value: viewModel.maxCashDriftPercent});
 
-		ko.applyBindings(viewModel);
+			ko.applyBindings(viewModel);
 
-		jQuery("#submitCreateAccount").on("click", Poptart.Monitor.Account.CreateAccount.submit.bind(this));
+			jQuery("#createAccountHoldings").igGrid(setGridOptions.call({
+				primaryKey: "pk",
+				dataSource: [],
+				dataSourceType: "json"
+			}));
+
+			jQuery("#submitCreateAccount").on("click", Poptart.Monitor.Account.CreateAccount.submit.bind(this));
+		};
+		jQuery.ig.loader(loaderConfig);
 	};
 
 	ReturnObj.submit = function() {
