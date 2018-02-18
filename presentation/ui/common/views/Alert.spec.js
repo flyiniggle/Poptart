dc6922ff483dae27e5fb855dcd0bb7d5a86caaf9
Dan Thompson <d.thompso@yahoo.com>
2018-02-18 17:56:46 -0500

    more alert tests

--- presentation/ui/common/views/Alert.spec.js
+++ presentation/ui/common/views/Alert.spec.js
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import Alert from './Alert';
 
 describe("#Alert", function() {
-	it("should show default values", function() {
+	it("should show default values.", function() {
 		const vm = mount(Alert);
 
 		expect(vm.find(".info")).to.exist;
@@ -11,7 +11,7 @@ describe("#Alert", function() {
 		expect(vm.find(".alert_message").element.innerHTML).to.equal("Something isn't quite right.");
 	});
 
-	it("should show as informational", function() {
+	it("should show as informational.", function() {
 		const vm = mount(Alert, {
 				propsData: {
 					level: "info"
@@ -21,7 +21,7 @@ describe("#Alert", function() {
 		expect(vm.find(".info")).to.exist;
 	});
 
-	it("should show as warning", function() {
+	it("should show as warning.", function() {
 		const vm = mount(Alert, {
 			propsData: {
 				level: "warning"
@@ -31,7 +31,7 @@ describe("#Alert", function() {
 		expect(vm.find(".warning")).to.exist;
 	});
 
-	it("should show as an error", function() {
+	it("should show as an error.", function() {
 		const vm = mount(Alert, {
 			propsData: {
 				level: "error"
@@ -39,5 +39,27 @@ describe("#Alert", function() {
 		});
 
 		expect(vm.find(".error")).to.exist;
+	});
+
+	it("should show a message.", function() {
+		const message = "This is a message!";
+		const vm = mount(Alert, {
+			propsData: {
+				message
+			}
+		});
+
+		expect(vm.find(".alert_message").element.innerHTML).to.equal(message);
+	});
+
+	it("should show a summary.", function() {
+		const summary = "This is a summary!";
+		const vm = mount(Alert, {
+			propsData: {
+				summary
+			}
+		});
+
+		expect(vm.find(".alert_summary").element.innerHTML).to.equal(summary);
 	});
 })
