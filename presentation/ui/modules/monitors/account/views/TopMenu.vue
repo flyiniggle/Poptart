0bd70be2ea8748e882d62c418773cff1a786fd54
Dan Thompson <dthompson@vestmark.com>
2018-03-02 09:58:24 -0500

    just to shake things up a bit, no more default selected menu on account monitor

--- presentation/ui/modules/monitors/account/views/TopMenu.vue
+++ presentation/ui/modules/monitors/account/views/TopMenu.vue
@@ -24,7 +24,7 @@
 		},
 		render: function(h) {
 			return (
-				<top-menu startingSelectedRibbon="managementMenu">
+				<top-menu startingSelectedRibbon="">
 					<div slot="navigatorControls">
 						<a href="/">Dashboard</a>
 					</div>
