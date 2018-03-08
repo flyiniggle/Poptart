37243b6304a8814cc20df47aa18b1bac33418839
Dan Thompson <d.thompso@yahoo.com>
2018-03-08 12:53:56 -0500

    another test

--- presentation/ui/common/views/Utils.test.js
+++ presentation/ui/common/views/Utils.test.js
@@ -4,17 +4,24 @@ import * as Utils from "./Utils";
 
 describe("#Poptart #Common #Views #Utils", function() {
 	describe("#registerLocalComponent", function() {
+		const fakeComponent = {
+			$options: {
+				components: {}
+			}
+		};
+
 		it("should add new components to a component's list of registered sub-components.", function () {
-			const fakeComponent = {
-				$options: {
-					components: {}
-				}
-			};
 			const fakeSubComponent = {};
 			const subcomponents = { test: fakeSubComponent };
 
 			Utils.registerLocalComponent(fakeComponent, subcomponents);
 			assert.include(fakeComponent.$options.components, { test: fakeSubComponent });
+		});
+
+		it("should support partial application.", function() {
+			const registerer = Utils.registerLocalComponent(fakeComponent);
+
+			assert.isFunction(registerer);
 		});
 	});
 });
