fa6e3480671bbe7a06ff1f356e9544d39ea03a41
Dan Thompson <d.thompso@yahoo.com>
2018-03-08 23:57:09 -0500

    security monitor sans igloader

--- presentation/ui/modules/monitors/security/scripts/Poptart.Monitor.Security.js
+++ presentation/ui/modules/monitors/security/scripts/Poptart.Monitor.Security.js
@@ -1,9 +1,15 @@
 // Poptart.Monitor.Security
 ////////////////////////////////
-import jQuery from "Lib/Poptart.jQuery";
 import Vue from "vue";
+import jQuery from "Lib/Poptart.jQuery";
+import "jqueryui";
+import "@infragistics/ignite-ui-full/en/js/infragistics.core";
+//import "@infragistics/ignite-ui-full/en/js/infragistics.dv";
+//import "@infragistics/ignite-ui-full/en/js/infragistics.lob";
+import "@infragistics/ignite-ui-full/en/js/modules/infragistics.datasource";
+import "@infragistics/ignite-ui-full/en/js/modules/infragistics.ui.grid.framework";
+import "@infragistics/ignite-ui-full/en/js/modules/infragistics.ui.grid.shared";
 
-import { loaderConfig, loader } from "Lib/Poptart.Ignite";
 import TopMenu from "Poptart/common/views/TopMenu/TopMenu"
 
 import "Poptart/css/main";
@@ -12,26 +18,18 @@ import "Poptart/css/main";
 var securities = [];
 
 function init() {
-	let configInstance = Object.create(loaderConfig, {});
-
-	configInstance.resources = "igGrid";
-	configInstance.ready = function() {
-		var table = jQuery("#sm_table");
-
-		table.igGrid({
-			dataSource: securities,
-			width: "100%",
-			columns: [
-				{headerText: "#", key: "pk", dataType: "integer", width: "40px"},
-				{headerText: "Security", key: "ticker", dataType: "string", width: "150px"},
-				{headerText: "CUSIP", key: "CUSIP", dataType: "string", width: "150px"},
-				{headerText: "Description", key: "description", dataType: "string", width: "200px"},
-				{headerText: "Last Price", key: "last_price", dataType: "float", width: "100px"},
-				{headerText: "Asset Class", key: "segment", dataType: "float", width: "75px"}
-			]
-		});
-	};
-	loader(configInstance);
+	jQuery("#sm_table").igGrid({
+		dataSource: securities,
+		width: "100%",
+		columns: [
+			{headerText: "#", key: "pk", dataType: "integer", width: "40px"},
+			{headerText: "Security", key: "ticker", dataType: "string", width: "150px"},
+			{headerText: "CUSIP", key: "CUSIP", dataType: "string", width: "150px"},
+			{headerText: "Description", key: "description", dataType: "string", width: "200px"},
+			{headerText: "Last Price", key: "last_price", dataType: "float", width: "100px"},
+			{headerText: "Asset Class", key: "segment", dataType: "float", width: "75px"}
+		]
+	});
 };
 
 //Main
